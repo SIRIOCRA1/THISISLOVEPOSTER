7c2ae4bb42c46533d0c16d15a0298f1d24232279
Jc-Arroyo <jcarroyos@libertadores.edu.co>
2024-09-06 20:46:49 -0500

    Update ardapro.ino
    Esta versiòn ya esta revisada para la placa Esplora.
    
    exitos

--- ardapro/ardapro.ino
+++ ardapro/ardapro.ino
@@ -1,17 +1,19 @@
 // Código para Arduino Esplora
 
+#include <Esplora.h>  // Importa la librería Esplora
+
 // Definir el umbral de luz para decidir cuándo mostrar cada imagen
-const int THRESHOLD = 500; // Ajusta este valor según tus necesidades
+const int THRESHOLD = 500;  // Ajusta este valor según tus necesidades
 
 void setup() {
   Serial.begin(9600);  // Inicia la comunicación serial a 9600 baudios
 }
 
 void loop() {
-  int lightLevel = analogRead(A0); // Lee el valor del sensor de luz en el pin A0
+  int lightLevel = Esplora.readLightSensor();  // Lee el valor del sensor de luz
 
   // Enviar el valor del sensor al puerto serial
   Serial.println(lightLevel);
 
-  delay(100); // Espera 100 ms antes de leer nuevamente
+  delay(100);  // Espera 100 ms antes de leer nuevamente
 }
